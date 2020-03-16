524e885477e37e43fd9c9f9dd509e7bb271466ea
favor <favor_zlx@163.com>
2020-03-16 16:17:51 +0800

    第一次提交

---
+++ bpms2.0/bpms/bpms-permission-web/src/main/java/com/zlx/bpms/BpmsPermissionWebApplication.java
@@ -0,0 +1,13 @@
+package com.zlx.bpms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmsPermissionWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpmsPermissionWebApplication.class, args);
+    }
+
+}
